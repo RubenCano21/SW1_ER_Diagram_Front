47495c6cd9668969124c157bd9ac7b3279595370
RubenCano21 <ruben217.rc@gmail.com>
2025-09-30 13:43:51 -0400

    ultimos cambios

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,16 @@ const nextConfig: NextConfig = {
     removeConsole: process.env.NODE_ENV === 'production' ? {
       exclude: ['error', 'warn', 'info'] // Retain error, warn, and info logs in production
     } : false
-  }
+  },
+
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL
+  },
+
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
